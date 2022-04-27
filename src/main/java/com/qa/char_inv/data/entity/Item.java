b4a98e657b93e42b1342b308c387c5aacb7684dc
Allister William Gray Gardner <allister.w.g.gardner@gmail.com>
2022-04-27 20:24:14 +0100

    forgot to switch branches, added constructors and getters and setters for item

--- src/main/java/com/qa/char_inv/data/entity/Item.java
+++ src/main/java/com/qa/char_inv/data/entity/Item.java
@@ -3,15 +3,17 @@ package com.qa.char_inv.data.entity;
 import javax.validation.constraints.*;
 import javax.persistence.*;
 
+@Entity // specifies class is an entity
+@Table(name = "item") // specifies primary table for entity and names it
 public class Item {
 	
-	@Id
+	@Id // sets primary key
 	@Column(name = "id")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // acts as our AUTO INCREMENT in mySQL
 	private int id;
 	
-	@NotNull
-	@NotBlank
+	@NotNull // NOT NULL - must have an entry
+	@NotBlank // Entry cannot be blank (whitespace)
 	@Size(min = 3, max = 26, message = "please insert an item name between 3 and 26 characters long")
 	private String itemName;
 	
@@ -24,6 +26,39 @@ public class Item {
 	@NotBlank
 	@Max(value = 20, message = "If item is magic 'true' if not 'false'")
 	private boolean isMagic;
+
+	
+	//empty constructor
+	public Item() {
+		super();
+	}
+
+	
+	//full args constructor
+	public Item(int id,
+			@NotNull @NotBlank @Size(min = 3, max = 26, message = "please insert an item name between 3 and 26 characters long") String itemName,
+			@NotNull @NotBlank @Max(value = 200, message = "please provide a description of the item, less than 200 characters") String description,
+			@NotNull @NotBlank @Max(value = 20, message = "If item is magic 'true' if not 'false'") boolean isMagic) {
+		super();
+		this.id = id;
+		this.itemName = itemName;
+		this.description = description;
+		this.isMagic = isMagic;
+	}
+
+	
+	//constructor without id as this will be generated for us
+	public Item(
+			@NotNull @NotBlank @Size(min = 3, max = 26, message = "please insert an item name between 3 and 26 characters long") String itemName,
+			@NotNull @NotBlank @Max(value = 200, message = "please provide a description of the item, less than 200 characters") String description,
+			@NotNull @NotBlank @Max(value = 20, message = "If item is magic 'true' if not 'false'") boolean isMagic) {
+		super();
+		this.itemName = itemName;
+		this.description = description;
+		this.isMagic = isMagic;
+	}
+	
+	
 	
 
 }
