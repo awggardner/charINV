2388a33a1c8dcab861fb9ac64e704a72cb421640
Allister William Gray Gardner <allister.w.g.gardner@gmail.com>
2022-04-27 19:14:50 +0100

    added fields in entities

--- src/main/java/com/qa/char_inv/entity/Inventory.java
+++ src/main/java/com/qa/char_inv/entity/Inventory.java
@@ -1,5 +1,16 @@
 package com.qa.char_inv.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "inventory")
 public class Inventory {
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	
 
 }
--- src/main/java/com/qa/char_inv/entity/Item.java
+++ src/main/java/com/qa/char_inv/entity/Item.java
@@ -1,5 +1,29 @@
 package com.qa.char_inv.entity;
 
+import javax.validation.constraints.*;
+import javax.persistence.*;
+
 public class Item {
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@NotNull
+	@NotBlank
+	@Size(min = 3, max = 26, message = "please insert an item name between 3 and 26 characters long")
+	private String itemName;
+	
+	@NotNull
+	@NotBlank
+	@Max(value = 200, message = "please provide a description of the item, less than 200 characters")
+	private String description;
+	
+	@NotNull
+	@NotBlank
+	@Max(value = 20, message = "If item is magic 'true' if not 'false'")
+	private boolean isMagic;
+	
 
 }
