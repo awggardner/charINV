0fb70269786ee1db241d4777a8dee845171a07e9
Allister William Gray Gardner <allister.w.g.gardner@gmail.com>
2022-04-28 16:26:53 +0100

    many to many added to inventory, many to many now done

--- src/main/java/com/qa/char_inv/data/entity/Inventory.java
+++ src/main/java/com/qa/char_inv/data/entity/Inventory.java
@@ -1,5 +1,8 @@
 package com.qa.char_inv.data.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.*;
 
 @Entity // specifies class is an entity
@@ -10,11 +13,19 @@ public class Inventory {
 	@Column(name = "id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO INCREMENT
 	private int id;
-
-	//empty constructor
-	public Inventory() {
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "inventory_items", // name of the join table
+	           joinColumns = @JoinColumn(name = "inventory_id", referencedColumnName = "id"), // name of the key of the domain which owns the relationship
+	           inverseJoinColumns = @JoinColumn(name = "item_id", referencedColumnName = "id")) // inverse of above
+	private List<Item> items;
+	
+	
+	protected Inventory() {
 		super();
+		this.items = new ArrayList<>();
 	}
+
 
 	public Inventory(int id) {
 		super();
@@ -27,6 +38,20 @@ public class Inventory {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+	
+	public List<Item> getTags() {
+		return items;
+	}
+	
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Inventory [id=" + id + ", items=" + items + "]";
 	}
 	
 	
