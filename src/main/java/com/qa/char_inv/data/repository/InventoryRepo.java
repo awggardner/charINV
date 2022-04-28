b8160a0e223c01ed4893ee9b0e62b2126430de6c
Allister William Gray Gardner <allister.w.g.gardner@gmail.com>
2022-04-28 14:22:13 +0100

    added inventory repo

---
+++ src/main/java/com/qa/char_inv/data/repository/InventoryRepo.java
@@ -0,0 +1,9 @@
+package com.qa.char_inv.data.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.qa.char_inv.data.entity.Inventory;
+
+public interface InventoryRepo extends JpaRepository<Inventory, Integer>{
+
+}
