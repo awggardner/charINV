89ac40f72e8062d991f29cd18a33ed482b2e9919
Allister William Gray Gardner <allister.w.g.gardner@gmail.com>
2022-04-30 22:06:12 +0100

    added create inventory in inventory service

--- src/main/java/com/qa/char_inv/service/InventoryService.java
+++ src/main/java/com/qa/char_inv/service/InventoryService.java
@@ -5,8 +5,12 @@ import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.qa.char_inv.data.dto.InventoryDTO;
+import com.qa.char_inv.data.dto.NewInventoryDTO;
 import com.qa.char_inv.data.entity.Inventory;
 import com.qa.char_inv.data.repository.InventoryRepo;
 
@@ -14,11 +18,28 @@ import com.qa.char_inv.data.repository.InventoryRepo;
 public class InventoryService {
 
 	private InventoryRepo inventoryRepo;
+	private ModelMapper modelMapper;
 	
 	@Autowired 
-	public InventoryService(InventoryRepo inventoryRepo) {
+	public InventoryService(InventoryRepo inventoryRepo, ModelMapper modelMapper) {
+		super();
 		this.inventoryRepo = inventoryRepo;
+		this.modelMapper = modelMapper;
 	}
+	
+	// inventory toDTO
+	private InventoryDTO toDTO(Inventory inventory) {
+		return modelMapper.map(inventory, InventoryDTO.class);
+	}
+	
+	// create inventory
+	public InventoryDTO createInventory(NewInventoryDTO inventoryDTO) {
+		Inventory create = this.modelMapper.map(inventoryDTO, Inventory.class);
+		Inventory newInventory = inventoryRepo.save(create);
+		return this.toDTO(newInventory);
+	}
+	
+	
 	
 	public List<Inventory> readAll() {
 		return inventoryRepo.findAll();
